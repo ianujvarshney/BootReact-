e703261685fc98cde101b11c17af939b77f3006f
ianujvarshney <anujkk19111997@gmail.com>
2021-12-04 16:34:50 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,90 +1,17 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { Button, Alert, Row, Col, Navbar, Nav, NavDropdown, Form, Container, FormControl } from 'react-bootstrap';
-
 import './App';
 
-function AlertDismissibleExample() {
-  const [show, setShow] = useState(true);
-  if (show) {
-    return (
-      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-        <p>
-          Change this and that and try again. Duis mollis, est non commodo
-          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-          Cras mattis consectetur purus sit amet fermentum.
-        </p>
-      </Alert>
-    );
+function App() {
+  function apple()
+  {
+      <h1>hello worl</h1>
   }
-  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
-}
-
-function App()
-{
-  return (   
+  return (
     <div className="App">
-      <div className="container">
-        <section>
-          <Button variant="primary">Primary</Button>{' '}
-          <Button variant="secondary">Secondary</Button>{' '}
-          <Button variant="success">Success</Button>{' '}
-          <Button variant="warning">Warning</Button>{' '}
-          <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-          <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-          <Button variant="link">Link</Button>
-        </section>
-        <section>
-        <Navbar bg="light" expand="lg">
-    <Container fluid>
-      <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
-        <Nav
-          className="me-auto my-2 my-lg-0"
-          style={{ maxHeight: '100px' }}
-          navbarScroll
-        >
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Link</Nav.Link>
-          <NavDropdown title="Link" id="navbarScrollingDropdown">
-            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action5">
-              Something else here
-            </NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link href="#" disabled>
-            Link
-          </Nav.Link>
-        </Nav>
-        <Form className="d-flex">
-          <FormControl
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Container>
-</Navbar>
-          </section>
-        <section>
-          <Row className="mx-0">
-            <Button as={Col} variant="primary">Button #1</Button>
-            <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
-            <Button as={Col} variant="success">Button #3</Button>
-          </Row>
-        </section>
-          <h1>React + Bootstrap Class</h1>
-          <AlertDismissibleExample/>
-          <button className="btn btn-primary">Primary</button>
-      </div>
+        <button onClick={<h1>Hello world</h1>}>Click Me</button>
     </div>
-    );
+  );
 }
 
 
