9165757fa40985515da5c23c7ea906ea7e2397cf
ianujvarshney <anujkk19111997@gmail.com>
2021-12-05 15:48:15 +0530

    latest change in files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import { Button, Alert, Row, Col, Navbar, Nav, NavDropdown, Form, Container, FormControl } from 'react-bootstrap';
 import './App';
+import Navbar from 
+import './App.css';
+
 
 function App() {
+  const [data, setdata] = useState(0);
   function apple()
   {
-      <h1>hello worl</h1>
+    setdata(data+1);
   }
-  return (
-    <div className="App">
-        <button onClick={<h1>Hello world</h1>}>Click Me</button>
-    </div>
-  );
+  return <>
+    <h1 className="text-center" style={heading}> Hello React Developer </h1>
+  </>
 }
 
 
